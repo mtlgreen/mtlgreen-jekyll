b687e12203dfd06f589fe24de6a234f1e7b577b9
Paul Cuciureanu <paulcu@gmail.com>
2017-02-12 15:27:25 -0500

    Slimmed down import

--- import-from-google-sites.sh
+++ import-from-google-sites.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 rm -rf en
-wget -P en -l1 --span-hosts -mkEpnp -nd https://sites.google.com/view/mtlgreen
+wget -D=sites.google.com,googleusercontent.com -P en -l1 --span-hosts -mkEpnp -e robots=off -nd https://sites.google.com/view/mtlgreen
 mv en/mtlgreen.html en/index.html
 
-rm -rf fr
-wget -P fr -l1 --span-hosts -mkEpnp -nd https://sites.google.com/view/vertmtl
-mv fr/vertmtl.html fr/index.html
+#rm -rf fr
+#wget -P fr -l1 --span-hosts -mkEpnp -nd https://sites.google.com/view/vertmtl
+#mv fr/vertmtl.html fr/index.html
