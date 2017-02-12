028d5ebd50498fdf9b6ce7fd651efd301fe28f7d
Paul Cuciureanu <paulcu@gmail.com>
2017-02-12 18:53:04 -0500

    Visible menu

--- import-from-google-sites.sh
+++ import-from-google-sites.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
 rm -rf en
-wget -D=sites.google.com,googleusercontent.com -P en -l1 --span-hosts -mkEpnp -e robots=off -nd https://sites.google.com/view/mtlgreen
+wget -D=gstatic.com,sites.google.com,googleusercontent.com -P en -l1 --span-hosts -mkEpnp -e robots=off -nd https://sites.google.com/view/mtlgreen
 mv en/mtlgreen.html en/index.html
-
-rm -rf fr
-wget -P fr -l1 --span-hosts -mkEpnp -nd https://sites.google.com/view/vertmtl
-wget -D=sites.google.com,googleusercontent.com -P fr -l1 --span-hosts -mkEpnp -e robots=off -nd https://sites.google.com/view/vertmtl
-mv fr/vertmtl.html fr/index.html
+sed -i -- 's/visibility: hidden/visibility: visible/g' en/index.html
