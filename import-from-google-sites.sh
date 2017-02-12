e3986b3ebe2a956ec835e67875fa21c49fcccfd7
Paul Cuciureanu <paulcu@gmail.com>
2017-02-12 17:40:52 -0500

    Newest copy

--- import-from-google-sites.sh
+++ import-from-google-sites.sh
@@ -4,6 +4,7 @@ rm -rf en
 wget -D=sites.google.com,googleusercontent.com -P en -l1 --span-hosts -mkEpnp -e robots=off -nd https://sites.google.com/view/mtlgreen
 mv en/mtlgreen.html en/index.html
 
-#rm -rf fr
-#wget -P fr -l1 --span-hosts -mkEpnp -nd https://sites.google.com/view/vertmtl
-#mv fr/vertmtl.html fr/index.html
+rm -rf fr
+wget -P fr -l1 --span-hosts -mkEpnp -nd https://sites.google.com/view/vertmtl
+wget -D=sites.google.com,googleusercontent.com -P fr -l1 --span-hosts -mkEpnp -e robots=off -nd https://sites.google.com/view/vertmtl
+mv fr/vertmtl.html fr/index.html
