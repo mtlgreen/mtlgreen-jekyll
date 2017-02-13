90c8dd425138b0916e54463eada92998d3aa419f
Paul Cuciureanu <paulcu@gmail.com>
2017-02-12 19:29:04 -0500

    Fixing links hack

--- import-from-google-sites.sh
+++ import-from-google-sites.sh
@@ -4,3 +4,5 @@ rm -rf en
 wget -D=gstatic.com,sites.google.com,googleusercontent.com -P en -l1 --span-hosts -mkEpnp -e robots=off -nd https://sites.google.com/view/mtlgreen
 mv en/mtlgreen.html en/index.html
 sed -i -- 's/visibility: hidden/visibility: visible/g' en/index.html
+sed -i -- 's|title="Green Challenge"|onclick="location.replace(\x27https://sites.google.com/view/mtlgreen/green-challenge\x27)" title="Green Challenge"|g' en/index.html
+sed -i -- 's|title="Sponsors"|onclick="location.replace(\x27https://sites.google.com/view/mtlgreen/sponsors\x27)" title="Green Challenge"|g' en/index.html
